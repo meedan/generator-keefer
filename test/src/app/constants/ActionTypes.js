19cd6f0b66c82b85aa55004e3b32c62061c0a05b
Caio SBA <caiosba@gmail.com>
2016-03-14 01:23:20 -0300

    Removing the example application

--- test/src/app/constants/ActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const NOTIFY_SEND = 'notify/SEND';
-export const NOTIFY_RECEIVE = 'notify/RECEIVE';
-export const ERROR = 'ERROR';
--- test/src/app/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import app from './app';
-import extension from './extension';
-
-export default combineReducers({ app, extension });
